3031a5be54b4a63fb3e73aaf62cdefb69005128f
julietjustin <julietkurian999@gmail.com>
2022-10-12 18:41:17 +0530

    New Java

---
+++ IfElseExampleWithIntegers.java
@@ -0,0 +1,18 @@
+class IfElseExampleWithIntegers
+{
+	public static void main(String args[])
+		{
+		int a = Integer.parseInt(args[0]);
+			{
+			if (a > 10 )
+				{
+				System.out.println("The Number is greater than 10");
+				}
+			else if (a < 10)
+			    {
+				System.out.println("The Number is smaller than 10");
+				}
+			}
+		}
+}
+				
